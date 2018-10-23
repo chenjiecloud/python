b2c336b5ce6b2331e3444bab82e4c92fc136b7a7
chenjie <chenjiecloud@gmail.com>
2018-10-23 23:15:51 +0800

    set

--- set.py
+++ set.py
@@ -16,5 +16,35 @@ print(s) # {'j', 'c', 'l', ' ', 'k'}
 # 使用in，not in 来判断是否存在于集合
 
 # 更新集合
+# add()
+
+# 作为序列添加（去重）
+# update
+
+
+a = set([1, 2, 3, 4, 5])
+b = set([4, 5, 6, 7, 8])
+
+
+print(a.intersection(b)) # 交集
+print(a.union(b)) # 并集
+print(a.difference(b)) # 单向差集（a里面有，b里面没有）a - b
+print(a.symmetric_difference(b)) # 对称差集 （反向交集）
+
+
+# | 求并集
+# - 求差集
+# ^ 对称差集
+# & 求交集
+
+# 父集(超集)
+print(a.issuperset(b))
+
+# 子集
+print(a.issubset(b))
+
+
+
+
 
 
