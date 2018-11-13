419d0ee1159337a4e8d3977c4ba84ef22074da96
chenjie <chenjiecloud@gmail.com>
2018-11-13 22:30:02 +0800

    add logginh

--- logging模块.py
+++ logging模块.py
@@ -3,8 +3,14 @@
 
 # 日志模块
 
+
 import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='/tmp/test.log',
+                    filemode='w')
 
 logging.debug('debug message')
 logging.info('info message')
